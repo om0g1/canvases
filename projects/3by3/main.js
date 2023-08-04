d609bb8ad7e70be2b900ec72268708051d2e5336
om0g1 <omogidavis@gmail.com>
2023-08-04 18:10:45 +0300

    v0.0.1

---
+++ projects/3by3/main.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("canvas");
+const pen = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function drawGrid() {
+    pen.clearRect(0, 0, canvas.width, canvas.height);
+
+    pen.beginPath();
+    for(let i = canvas.width/3; i < canvas.width; i+= canvas.width/3) {
+        pen.moveTo(i, 0);
+        pen.lineTo(i, canvas.height);
+    }
+    for(let i = canvas.height/3; i < canvas.height; i+=canvas.height/3) {
+        pen.moveTo(0, i);
+        pen.lineTo(canvas.width, i);
+    }
+    pen.stroke();
+}
+
+drawGrid();
